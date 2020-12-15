48c594b779806db502096d9de3f4e8c201defc70
Revoir <dezudakiddo@hotmail.com>
2020-12-15 08:38:20 -0500

    zoom

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+$(window).resize(function() {
+    window.zoom();
+});
+
+function zoom() {
+    var altura = window.innerHeight,
+      ancho = window.innerWidth,
+      alturaDocumento = parseInt($("body").css("height")),
+      anchoDocumento = parseInt($("body").css("width")),
+      zoom,
+      html = $("body, .queryloader__overlay");
+    if (altura <= ancho) {
+      zoom = altura / alturaDocumento;
+    } else {
+      zoom = ancho / anchoDocumento;
+    }
+    var anchoActual = anchoDocumento * zoom;
+    if (anchoActual >= ancho) {
+      zoom = ancho / anchoDocumento;
+    }
+    window.valueDraw = anchoDocumento / ancho;
+    var anchoReal = anchoDocumento * zoom;
+    var marginLeft = (ancho - anchoReal) / 2 +'px';
+    var alturaReal = alturaDocumento * zoom;
+    var marginTop = (altura - alturaReal) / 2 +'px';
+    var scale= "scale(" + zoom + ")";
+    $(html).css({"margin-left": marginLeft, "transform": scale, "margin-top": marginTop, 'transform-origin': '0px 0px'});
+    if (bowser.gecko) {
+      $(html).css({"-moz-transform-origin": "0 0 0", "-moz-transform": scale});
+    }
+  }
+
+
+
+
+
 $(document).ready(function() {
   
     TweenMax.set($('#logo'),{y:-50});
