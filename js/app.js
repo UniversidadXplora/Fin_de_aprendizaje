7af908b225c0afc9f9911956fc931d8e967a84f8
Revoir <dezudakiddo@hotmail.com>
2020-12-15 01:05:17 -0500

    arreglos diseño

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     TweenMax.set($('#logro1, #logro2, #logro3, #logro4, #logro5, #logro6'),{y:-100});
 
     
-    TweenMax.to($('#logro1, #logro3, #logro5'), 2,{rotation: 1, yoyo: true, repeat: -1, transformOrigin:"top", ease: Power1.easeInOut});
-    TweenMax.to($('#logro2, #logro4, #logro6'), 2.5,{rotation: -1, yoyo: true, repeat: -1, transformOrigin:"top", ease: Power1.easeInOut});
+ /*    TweenMax.to($('#logro1, #logro3, #logro5'), 2,{rotation: 1, yoyo: true, repeat: -1, transformOrigin:"top", ease: Power1.easeInOut});
+    TweenMax.to($('#logro2, #logro4, #logro6'), 2.5,{rotation: -1, yoyo: true, repeat: -1, transformOrigin:"top", ease: Power1.easeInOut}); */
 
     
 
